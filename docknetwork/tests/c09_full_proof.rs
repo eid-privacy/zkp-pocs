49d281e909f998beb9387bb063266b22644633fc
Linus Gasser <linus.gasser@epfl.ch>
2025-12-19 15:10:37 +0100

    WIP

--- docknetwork/tests/c09_full_proof.rs
+++ docknetwork/tests/c09_full_proof.rs
@@ -4,5 +4,24 @@ use docknetwork::*;
 
 #[test]
 fn c09_proof_full() -> Result<(), Box<dyn Error>> {
+    let mut common = Common::new();
+    let message = VerifierMessage::new("Verifier");
+
+    // Holder creates a presentation with a proof of knowledge and no revealed messages.
+    let presentation = common
+        .holder_bbs_presentation(message.clone(), vec![VerifiedCredential::FIELD_FIRSTNAME])
+        .close();
+
+    // Holder sends the proof to the verifier, which checks it.
+    common.verifier_check_proof_bbs(&message, &presentation);
+    assert_eq!(
+        presentation.message_string(VerifiedCredential::FIELD_FIRSTNAME),
+        Some(common.credential.message_strings[&VerifiedCredential::FIELD_FIRSTNAME].clone())
+    );
+    assert_eq!(
+        presentation.message_string(VerifiedCredential::FIELD_LASTNAME),
+        None
+    );
+
     Ok(())
 }
